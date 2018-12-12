92f27a243955ec24146b1f9a4daf379ae4e0bdc1
kkaczor <kkaczor@visionapartments.com>
2018-12-12 12:09:50 +0100

    dodany test selenium

--- autojava2/src/test/java/selenium/basic/SeleniumTests.java
+++ autojava2/src/test/java/selenium/basic/SeleniumTests.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -20,6 +21,8 @@ public class SeleniumTests {
     public void startDriver() {
         System.setProperty("webdriver.chrome.driver", "c:\\selenium\\chromedriver.exe");
         driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
     }
 
     @Test
@@ -37,14 +40,37 @@ public class SeleniumTests {
                 .filter(r->r.findElement(By.cssSelector("div.r > a")).getAttribute("href").equals("http://agileszkolenia.pl/"));
         List<WebElement> resultsList = resultFiltered.collect(Collectors.toList());
 
-        Assertions.assertEquals(2, resultsList.size());
+        Assertions.assertEquals(1, resultsList.size());
 
         WebElement result = resultsList.get(0);
         Assertions.assertEquals("agileszkolenia.pl/", resultsList.get(0).findElement(By.tagName("cite")).getText());
-
         }
 
-        @AfterEach
+    @Test
+    public void verifyAuthorOfBlogNoteAboutTransformations(){
+        driver.get("https://markowicz.pro/");
+        Stream<WebElement> listOfNotes = driver.findElements(By.className("entry-title"))
+                .stream()
+                .filter(n -> n.findElement(By.tagName("a")).getText().equals("O transformacjach"));
+
+        List<WebElement> filteredNotes = listOfNotes.collect(Collectors.toList());
+
+        Assertions.assertEquals(1, filteredNotes.size(), "only one matching note is found on main page");
+
+        driver.findElement(By.cssSelector("#eu-cookie-law input")).submit();
+
+        filteredNotes.get(0).click();
+
+        WebElement author = driver.findElement(By.cssSelector(".author > a"));
+
+        Assertions.assertEquals("Rafał", author.getText(), "Proper author name is displayed");
+        Assertions.assertEquals("https://markowicz.pro/author/rafal-markowicz/",
+                author.getAttribute("href"),
+                "Valid author URL is displayed");
+
+    }
+
+    @AfterEach
     public void closeDriver() {
         driver.quit();
         }
